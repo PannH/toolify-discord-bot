658d15570f277297b9deb0bca289626716903ae9
PannH <pannh2609@gmail.com>
2023-02-11 17:51:31 +0100

    fix: typo

--- src/functions/randomInteger.ts
+++ src/functions/randomInteger.ts
@@ -1,5 +1,5 @@
 export default function randomInteger(min: number, max: number): number {
 
-   return Math.floor(Math.random() * ((max - 1) - min)) + min;
+   return Math.floor(Math.random() * ((max + 1) - min)) + min;
 
 }
