28259220f45665c6321354ee37f0273a84e85d17
PannH <pannh2609@gmail.com>
2023-02-12 22:40:52 +0100

    Create the /binary command

---
+++ src/handlers/commands/binary.ts
@@ -0,0 +1,92 @@
+import { SlashCommand } from '../../classes';
+import { codeBlock, ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { convert, type Temperature } from 'convert';
+
+export default new SlashCommand({
+   name: 'binary',
+   description: 'Encode / decode binary.',
+   options: [{
+      name: 'encode',
+      description: 'Encode text to binary.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [{
+         name: 'text',
+         description: 'The text to encode to binary.',
+         type: ApplicationCommandOptionType.String,
+         required: true
+      }]
+   }, {
+      name: 'decode',
+      description: 'Decode binary to text.',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [{
+         name: 'binary',
+         description: 'The binary code you want to decode.',
+         type: ApplicationCommandOptionType.String,
+         required: true
+      }]
+   }]
+}, async (client, interaction) => {
+
+   const subcommand = interaction.options.getSubcommand();
+
+   switch (subcommand) {
+
+      case 'encode': {
+
+         const string = interaction.options.getString('text');
+
+         const binaryString = 
+            string
+               .split('')
+               .map((character) => character.charCodeAt(0).toString(2))
+               .join(' ');
+
+         const binaryEncoderEmbed = new EmbedBuilder()
+            .setColor(0x2f3136)
+            .setTitle('Binary Encoder')
+            .setDescription(
+               codeBlock(string) +
+               codeBlock(`= ${binaryString}`)
+            );
+
+         await interaction.reply({
+            embeds: [binaryEncoderEmbed]
+         });
+
+         break;
+
+      }
+
+      case 'decode': {
+
+         const binaryString = interaction.options.getString('binary');
+
+         const string =
+            binaryString
+               .split(' ')
+               .map((binaryPart) => String.fromCharCode(parseInt(binaryPart, 2)))
+               .join('');
+
+         const binaryEncoderEmbed = new EmbedBuilder()
+            .setColor(0x2f3136)
+            .setTitle('Binary Decoder')
+            .setDescription(
+               codeBlock(binaryString) +
+               codeBlock(`= ${string}`)
+            );
+
+         await interaction.reply({
+            embeds: [binaryEncoderEmbed]
+         });
+
+         break;
+
+      }
+
+      default:
+         break;
+
+   }
+
+});
