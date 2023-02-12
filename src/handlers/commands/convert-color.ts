8b231d6c88bdd19bfe9d6464ddcd7be9f16cb5ae
PannH <pannh2609@gmail.com>
2023-02-12 16:28:27 +0100

    Create the /convert-color command

---
+++ src/handlers/commands/convert-color.ts
@@ -0,0 +1,57 @@
+import { SlashCommand } from '../../classes';
+import tinycolor, { type Instance as ColorInstance } from 'tinycolor2';
+import { createCanvas } from 'canvas';
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, AttachmentBuilder, codeBlock, ApplicationCommandOptionType, ModalBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
+import { randomUUID } from 'crypto';
+import { minutesToMilliseconds } from 'date-fns';
+
+export default new SlashCommand({
+   name: 'convert-color',
+   description: 'Convert a color to HEX, RGB, HSL and HSV formats.',
+   options: [{
+      name: 'color',
+      description: 'The color you want to convert.',
+      type: ApplicationCommandOptionType.String,
+      required: true
+   }]
+}, async (client, interaction) => {
+
+   const color = tinycolor(
+      interaction.options.getString('color')
+   );
+   if (!color.isValid())
+      return interaction.reply({
+         content: 'The color you specified is invalid.',
+         ephemeral: true
+      });
+
+   await interaction.deferReply();
+
+   const WIDTH = 300;
+   const HEIGHT = 50;
+
+   const canvas = createCanvas(WIDTH, HEIGHT);
+   const ctx = canvas.getContext('2d');
+
+   ctx.fillStyle = color.toHexString();
+   ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+   const colorImageAttachment = new AttachmentBuilder(canvas.toBuffer(), { name: 'color.png' });
+
+   const colorValuesEmbed = new EmbedBuilder()
+      .setColor(0x2f3136)
+      .setTitle('Color Converter')
+      .setDescription(
+         codeBlock(`HEX | ${color.toHexString()}`) +
+         codeBlock(`RGB | ${color.toRgbString()}`) +
+         codeBlock(`HSL | ${color.toHslString()}`) +
+         codeBlock(`HSV | ${color.toHsvString()}`)
+      )
+      .setImage('attachment://color.png');
+
+   await interaction.editReply({
+      embeds: [colorValuesEmbed],
+      files: [colorImageAttachment]
+   });
+
+});
