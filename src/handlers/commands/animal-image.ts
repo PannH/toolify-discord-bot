f9a90936d3755fd34cba0fcf70e304f69edd5208
PannH <pannh2609@gmail.com>
2023-02-18 13:49:46 +0100

    Add a regenerate button

--- src/handlers/commands/animal-image.ts
+++ src/handlers/commands/animal-image.ts
@@ -1,7 +1,9 @@
 import { SlashCommand } from '../../classes';
-import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, EmbedBuilder } from 'discord.js';
 import axios from 'axios';
 import Constants from '../../utils/Constants';
+import { minutesToMilliseconds } from 'date-fns';
+import { randomUUID } from 'crypto';
 
 export default new SlashCommand({
    name: 'animal-image',
@@ -45,15 +47,59 @@ export default new SlashCommand({
 
    await interaction.deferReply();
 
-   const { data } = await axios.get(`https://some-random-api.ml/animal/${subcommand}`);
+   const componentIds = {
+      'REGENERATE': randomUUID()
+   };
 
-   const randomAnimalImageEmbed = new EmbedBuilder()
-      .setColor(Constants.EMBED_COLOR)
-      .setTitle(`Random Animal Image Generator (${subcommand})`)
-      .setImage(data.image);
+   const regenerateImageAndUpdateMessage = new ActionRowBuilder()
+      .setComponents(
+         new ButtonBuilder()
+            .setCustomId(componentIds['REGENERATE'])
+            .setLabel('Regenerate')
+            .setStyle(ButtonStyle.Primary)
+      );
 
-   await interaction.editReply({
-      embeds: [randomAnimalImageEmbed]
+   async function regenerateAndUpdateMessage(): Promise<void> {
+
+      const { data } = await axios.get(`https://some-random-api.ml/animal/${subcommand}`);
+   
+      const randomAnimalImageEmbed = new EmbedBuilder()
+         .setColor(Constants.EMBED_COLOR)
+         .setTitle(`Random Animal Image Generator (${subcommand})`)
+         .setImage(data.image);
+
+      await interaction.editReply({
+         embeds: [randomAnimalImageEmbed],
+         components: [regenerateImageAndUpdateMessage as any]
+      });
+
+   }
+
+   await regenerateAndUpdateMessage();
+
+   const componentCollector = interaction.channel.createMessageComponentCollector({
+      filter: (interaction) => Object.values(componentIds).includes(interaction.customId),
+      time: minutesToMilliseconds(15)
    });
+
+   componentCollector.on('collect', async (componentInteraction) => {
+
+      if (componentInteraction.user.id !== interaction.user.id)
+         return void componentInteraction.reply({
+            content: 'You must type the command yourself to use that.',
+            ephemeral: true
+         });
+      
+      if (componentInteraction.customId === componentIds['REGENERATE']) {
+         
+         await regenerateAndUpdateMessage();
+
+         await componentInteraction.deferUpdate();
+
+      }
+
+   });
+
+   componentCollector.on('end', () => {});
 
 });
