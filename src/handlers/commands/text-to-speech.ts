2d8f1820b859baba8950057f5e8dbc2c6e4c3772
PannH <pannh2609@gmail.com>
2023-02-18 14:05:03 +0100

    Create the /text-to-speech command

---
+++ src/handlers/commands/text-to-speech.ts
@@ -0,0 +1,47 @@
+import { SlashCommand } from '../../classes';
+import { codeBlock, ApplicationCommandOptionType, EmbedBuilder, AttachmentBuilder } from 'discord.js';
+import { getAudioUrl } from 'google-tts-api';
+import axios from 'axios';
+import Constants from '../../utils/Constants';
+
+export default new SlashCommand({
+   name: 'text-to-speech',
+   description: 'Convert a text to an audio file.',
+   options: [{
+      name: 'text',
+      description: 'The text you want to convert to audio.',
+      type: ApplicationCommandOptionType.String,
+      required: true
+   }]
+}, async (client, interaction) => {
+
+   const text = interaction.options.getString('text');
+
+   if (text.length > 200)
+      return interaction.reply({
+         content: 'The text cannot be longer than 200 characters.',
+         ephemeral: true
+      });
+
+   await interaction.deferReply();
+
+   const audioURL = getAudioUrl(text);
+   const { data: audioBuffer } = await axios.get(audioURL, {
+      responseType: 'arraybuffer'
+   });
+
+   const audioAttachment = new AttachmentBuilder(audioBuffer, { name: 'text_to_speech.mp3' });
+
+   const ttsEmbed = new EmbedBuilder()
+      .setColor(Constants.EMBED_COLOR)
+      .setTitle('Text To Speech')
+      .setDescription(
+         codeBlock(`Text | ${text}`)
+      )
+
+   await interaction.editReply({
+      embeds: [ttsEmbed],
+      files: [audioAttachment]
+   });
+
+});
